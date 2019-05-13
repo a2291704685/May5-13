88c47cdce5a0455a7bc181f80f0277d395b052cc
a2291704685 <2291704685@qq.com>
2019-05-13 15:46:15 +0800

    C++学习日志

---
+++ weather.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+int main(void){
+	
+	cout << "今天是2019/05/13 学习 C++课程的第15天 好好努力!" << endl;
+	cout << "今天是个很美好的一天，阳光明媚！" << endl;
+	
+	system("pause");
+	return 0;
+
+
+}
